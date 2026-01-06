9efade2043c264b59521d418bba0ddb3432ba35f
Abhinav Mishra <cseworld73@gmail.com>
2026-01-07 02:50:45 +0530

    update cors url

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ app.use(express.json())
 app.use(cors({
     origin: [
     "http://localhost:5173",
-    "https://movie-project-app2.netlify.app"
+    "https://jovial-baklava-8aa892.netlify.app"
   ],
   credentials: true
 }))
